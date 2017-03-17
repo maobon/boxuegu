74cce8b6c83a579a530b1dc7533d5b05e155d1f2
maobon <maobonhaha@gmail.com>
2017-03-17 21:20:41 +0800

    day55 晚 1.登录部分 2.保存用户信息到cookie

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?php
-
+// index.php 控制分发
 $path = 'index';
 $filename = 'index';
 
 if(array_key_exists('PATH_INFO',$_SERVER)){
+
     // 获取url中的路径
 	$pathinfo = $_SERVER['PATH_INFO'];
 	$pathinfo = explode('/', substr($pathinfo, 1));
+
     // 去掉路径中的第一个字符(/)
 	if(count($pathinfo) == 2) {
 		$path = $pathinfo[0];
 		$filename = $pathinfo[1];
 
-	}else{
-	    // 表示登录页面
+	} else {
+        // 表示登录页面
         if($pathinfo[0] == 'favicon.ico'){
-            header(200);
-            header('Content-Type','image/x-icon');
-            include './public/images/favicon.ico'; 
-            exit;
+           header(200);
+           header('Content-Type','image/x-icon');
+           include './public/images/favicon.ico';
+           exit;
         }
         $filename = 'login';
     }
+
+} else {
+    $filename = 'login';
 }
 
 // php的include 作用就是载入一个页面

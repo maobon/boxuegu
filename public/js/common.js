fba0a16c7e68650e9647415162bd107bea9bf14f
maobon <maobonhaha@gmail.com>
2017-03-17 23:49:58 +0800

    day55 晚 4. common.js 用户登录状态检测-存在Bug-试验无效

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,19 @@
+/**
+ * 控制左侧导航菜单的显示和隐藏
+ */
+$('.navs ul').prev('a').on('click', function () {
+    $(this).next().slideToggle();
+});
 
-	NProgress.start();
+/**
+ * 检测用户是否已经登录
+ * 没有登录的时 自动跳转到登录页面
+ *
+ * var pathname = location.pathname;
+ */
 
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+var flag = $.cookie('PHPSESSID');
+if (!flag) {
+    console.log("check PHPSESSID is none");
+    location.href = '/login';
+}

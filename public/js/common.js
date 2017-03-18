5607ddcbb4604fe18d06dec47964cf9d1a67d3e5
maobon <maobonhaha@gmail.com>
2017-03-18 23:29:10 +0800

    day56 晚 Login页面Bug修复 -> 修复common.js中的问题

--- public/js/common.js
+++ public/js/common.js
@@ -8,12 +8,11 @@ $('.navs ul').prev('a').on('click', function () {
 /**
  * 检测用户是否已经登录
  * 没有登录的时 自动跳转到登录页面
- *
- * var pathname = location.pathname;
  */
+var pathName = location.pathname;
+var isHasSESSID = $.cookie('PHPSESSID');
 
-var flag = $.cookie('PHPSESSID');
-if (!flag) {
-    console.log("check PHPSESSID is none");
+if (!isHasSESSID && pathName.indexOf("login") == -1) {
+    // cookie中没有SESSIONID && 路径中不包含"login"
     location.href = '/login';
 }

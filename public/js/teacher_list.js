982810a0a203f179816f4bcab2db4d4dbaec05d8
maobon <maobonhaha@gmail.com>
2017-03-20 00:11:37 +0800

    day57 晚 查询列表中某一位教师的详细信息

--- public/js/teacher_list.js
+++ public/js/teacher_list.js
@@ -4,7 +4,7 @@
  * 使用jquery发送ajax请求 对数据使用模板引擎渲染生成html 填充页面
  */
 
-define(['jquery', 'template'], function ($, template) {
+define(['jquery', 'template', 'bootstrap'], function ($, template) {
 
     $.ajax({
         type: 'get',
@@ -13,6 +13,33 @@ define(['jquery', 'template'], function ($, template) {
         success: function (data) {
             var strHTML = template('teacherlistTpl', {list: data.result});
             $("#teacherList").html(strHTML);
+
+            // $(".teacherOperation a").eq(0).click();
+            // $(".teacherOperation").find('a').eq(0).click();
+            // $(".teacherOperation a:eq(0)");
+            // $(".teacherOperation").find('a:eq(0)').click();
+
+            // 查看某一位教师的详情
+            // 需求: table中每个td中的第一个a
+            $("td.teacherOperation").find('a:eq(0)').click(function () {
+
+                var teacherId = $(this).parent("td").attr("data-tcid");
+                $.ajax({
+                    type: 'get',
+                    url: '/api/teacher/view',
+                    data: {tc_id: teacherId},
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.code == 200) {
+
+                            var strHTML = template('teacherDetailInfoTpl', data.result);
+                            $("#teacherDetailInfo").html(strHTML);
+                        }
+
+                    }
+                });
+            });
+
         }
     });
 

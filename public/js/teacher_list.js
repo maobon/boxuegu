e4ae26fe883e7561d1030813f75318a8b2d351be
maobon <maobonhaha@gmail.com>
2017-03-20 20:23:25 +0800

    day57 晚 讲师列表-讲师状态: 启用与注销

--- public/js/teacher_list.js
+++ public/js/teacher_list.js
@@ -2,6 +2,7 @@
  * Created by xinbob on 3/19/17.
  * 查询教师列表
  * 使用jquery发送ajax请求 对数据使用模板引擎渲染生成html 填充页面
+ * 功能点: 1.查看 2.编辑 3.注销
  */
 
 define(['jquery', 'template', 'bootstrap'], function ($, template) {
@@ -11,6 +12,7 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
         url: '/api/teacher',
         dataType: 'json',
         success: function (data) {
+
             var strHTML = template('teacherlistTpl', {list: data.result});
             $("#teacherList").html(strHTML);
 
@@ -57,7 +59,28 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
              */
             $("td.teacherOperation").find('a:eq(2)').click(function () {
                 var teacherId = $(this).parent('td').attr('data-tcid');
-                console.log(teacherId + " 注销 is clicked");
+                var td = $(this).parent('td');
+
+                var _this = this;
+                $.ajax({
+                    type: 'post',
+                    url: '/api/teacher/handle',
+                    data: {tc_id: teacherId, tc_status: td.attr('data-tcstatus')},
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.code == 200) {
+
+                            if (data.result.tc_status == 0) {
+                                $(_this).html("启 用");
+                            } else if (data.result.tc_status == 1) {
+                                $(_this).html("注 销");
+                            }
+
+                            td.attr('data-tcstatus', data.result.tc_status);
+                        }
+                    }
+                })
+
             });
 
         }

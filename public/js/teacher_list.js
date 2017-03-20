7bf70be800aa6c0de798757d43a49a9bfb05c0bb
maobon <maobonhaha@gmail.com>
2017-03-20 19:37:27 +0800

    day57 晚 查询教师的详细信息 Bug修复完成

--- public/js/teacher_list.js
+++ public/js/teacher_list.js
@@ -19,8 +19,11 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
             // $(".teacherOperation a:eq(0)");
             // $(".teacherOperation").find('a:eq(0)').click();
 
-            // 查看某一位教师的详情
-            // 需求: table中每个td中的第一个a
+            /**
+             * 查看按钮
+             * 查看某一位教师的详情
+             * 需求: table中每个td中的第一个a
+             */
             $("td.teacherOperation").find('a:eq(0)').click(function () {
 
                 var teacherId = $(this).parent("td").attr("data-tcid");
@@ -31,13 +34,30 @@ define(['jquery', 'template', 'bootstrap'], function ($, template) {
                     dataType: 'json',
                     success: function (data) {
                         if (data.code == 200) {
+                            // 渲染前可以调整数据格式
+                            data.result.tc_hometown = data.result.tc_hometown.split("|").join(" ");
 
                             var strHTML = template('teacherDetailInfoTpl', data.result);
                             $("#teacherDetailInfo").html(strHTML);
                         }
-
                     }
                 });
+            });
+
+            /**
+             * 编辑按钮
+             */
+            $("td.teacherOperation").find('a:eq(1)').click(function () {
+                var teacherId = $(this).parent('td').attr('data-tcid');
+                console.log(teacherId + " 编辑 is clicked");
+            });
+
+            /**
+             * 注销按钮
+             */
+            $("td.teacherOperation").find('a:eq(2)').click(function () {
+                var teacherId = $(this).parent('td').attr('data-tcid');
+                console.log(teacherId + " 注销 is clicked");
             });
 
         }
